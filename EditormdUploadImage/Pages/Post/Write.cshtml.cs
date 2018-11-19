fb6e04124d079d072a52c67456dec3fe52ffda11
Rector <lampo1024@qq.com>
2018-11-19 15:02:04 +0800

    upload image first version

---
+++ EditormdUploadImage/Pages/Post/Write.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EditormdUploadImage.Pages.Post
+{
+    public class WriteModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
